c1d3d35e95cb33104a312ea050b395df82c03918
sergio <dirtycrew666@gmail.com>
2022-08-19 15:26:13 +0300

    Put all components into App and connect local storage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Header from './components/Header';
+import FormGroup from './components/FormGroup';
+import TaskList from './components/TaskList';
+
 
 function App() {
+
+  useEffect(() => {
+    getFromLocalStorage()
+  }, [])
+
+  const [input, setInput] = useState('')
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    saveToLocalStorage()
+  })
+
+  const saveToLocalStorage = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }
+
+  const getFromLocalStorage = () => {
+    if (localStorage.getItem('tasks') === null){
+      localStorage.setItem('tasks', JSON.stringify([]))
+    } else {
+      let localTasks = JSON.parse(localStorage.getItem('tasks'))
+      setTasks(localTasks)
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Header />
+      <FormGroup input={input} setInput={setInput} tasks={tasks} setTasks={setTasks} />
+      <TaskList tasks={tasks} setTasks={setTasks} />
+    </div> 
   );
 }
 
